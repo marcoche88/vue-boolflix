0a36710d721835b668fd4d0546dc7f046e9ea135
marcoche88 <marco.che@hotmail.it>
2021-09-10 13:11:37 +0200

    added flag img

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -8,7 +8,13 @@
             <strong>Titolo originale:</strong> {{ result.original_title }}
           </li>
           <li>
-            <strong>Lingua originale:</strong> {{ result.original_language }}
+            <strong>Lingua originale: </strong>
+            <img
+              v-if="flagImg(result.original_language)"
+              :src="require(`../assets/img/${result.original_language}.png`)"
+              :alt="result.original_language"
+            />
+            <span v-else>{{ result.original_language }}</span>
           </li>
           <li><strong>Voto:</strong> {{ result.vote_average }}</li>
         </ul>
@@ -23,11 +29,20 @@ export default {
   props: {
     searchRes: Array,
   },
+  methods: {
+    flagImg(language) {
+      return language === "it" || language === "en";
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 ol > li {
   margin: 10px 0;
+}
+
+img {
+  width: 20px;
 }
 </style>
